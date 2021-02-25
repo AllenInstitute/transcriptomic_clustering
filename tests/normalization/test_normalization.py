7a4d8ef366cb8b5a546d2b2377f4d10ee54f5211
gnayuy <gnayuy@gmail.com>
2021-02-25 13:04:59 -0800

    remove schemas, change unittest to pytest, update docstring and add dense matrix support

--- tests/normalization/test_normalization.py
+++ tests/normalization/test_normalization.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 import numpy as np
 import anndata as ad
@@ -6,24 +6,24 @@ from scipy.sparse import csr_matrix
 
 from transcriptomic_clustering.normalization.normalize import normalize_cell_expresions
 
-class TestNormalization(unittest.TestCase):
+
+def test_normalization():
+    """
+        test normalization of cell expressions with a small example
+    """
+    arr = np.array([[ 6,  6,  4,  2, 18],
+                    [ 1,  0,  4,  8, 11],
+                    [ 6, 11,  3, 26, 26],
+                    [ 0,  0, 19,  9,  0]])
+
+    expected_norm = np.array([[17.3466147, 17.3466147, 16.7616566, 15.7616695, 18.9315715],
+                        [15.3466407,  0.0000000, 17.3466147, 18.3466104, 18.8060408],
+                        [16.3466234, 17.2210846, 15.3466407, 18.4620873, 18.4620873],
+                        [ 0.0000000,  0.0000000, 19.3721433, 18.2941431,  0.0000000]])
+
+    a_data = ad.AnnData(arr)
+    a_data.X = csr_matrix(a_data.X)
+    result = normalize_cell_expresions(a_data)
+    result.X = csr_matrix.toarray(result.X)
     
-    def test_normalization(self):
-        """
-            test normalization of cell expressions with a small example
-        """
-        arr = np.array([[ 6,  6,  4,  2, 18],
-                        [ 1,  0,  4,  8, 11],
-                        [ 6, 11,  3, 26, 26],
-                        [ 0,  0, 19,  9,  0]])
-
-        expected_norm = np.array([[17.35, 17.35, 16.76, 15.76, 18.93],
-                            [15.35,  0.  , 17.35, 18.35, 18.81],
-                            [16.35, 17.22, 15.35, 18.46, 18.46],
-                            [ 0. ,  0. , 19.37, 18.29,  0. ]])
-
-        a_data = ad.AnnData(arr)
-        a_data.X = csr_matrix(a_data.X)
-        result = normalize_cell_expresions(a_data)
-        result.X = csr_matrix.toarray(result.X)
-        self.assertAlmostEqual(result.X.all(), expected_norm.all())
+    np.testing.assert_almost_equal(result.X, expected_norm, decimal = 6)
--- transcriptomic_clustering/normalization/normalize.py
+++ transcriptomic_clustering/normalization/normalize.py
@@ -3,7 +3,6 @@ from typing import Dict, Optional, List, Any
 import logging
 import copy as cp
 
-from argschema.argschema_parser import ArgSchemaParser
 from transcriptomic_clustering.normalization._schemas import (
     InputParameters, OutputParameters)
 
@@ -11,25 +10,23 @@ import numpy as np
 import pandas as pd
 import anndata as ad
 import scanpy as sc
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, issparse
 
 
 def normalize_cell_expresions(cell_expressions: ad.AnnData):
     """
         Compute the normalization of cell expressions
 
-            (1) compute cpm (counts per million normalization): scanpy.pp.normalize_total,
-                which computes "single_expression/sum(all_expressions_per_cell)*1e6"
-            (2) compute log2: scipy.sparse.csr_matrix.log1p(x)/np.log(2.0),
-                which computes log(x+1)/log(2)
+            (1) compute cpm: count up the total reads in a sample and divide that number by 1,000,000
+            (2) compute log2: computes log(x+1)/log(2)
 
         Parameters
         ----------
-        cell_expressions: AnnData  
+        cell_expressions: input in AnnData format with sparse matrix (csr_matrix) 
 
         Returns
         -------
-        normalization result: AnnData
+        normalization result: output in AnnData format
 
     """
 
@@ -37,25 +34,10 @@ def normalize_cell_expresions(cell_expressions: ad.AnnData):
     sc.pp.normalize_total(cell_expressions, target_sum=1e6, inplace=True)
 
     # log
-    cell_expressions.X = csr_matrix.log1p(cell_expressions.X)/np.log(2.0)
+    if(issparse(cell_expressions.X)):
+        cell_expressions.X = csr_matrix.log1p(cell_expressions.X)/np.log(2.0)
+    else:
+        cell_expressions.X = np.log2(cell_expressions.X)
 
     return cell_expressions
 
-
-def main():
-    parser = ArgSchemaParser(
-        schema_type=InputParameters,
-        output_schema_type=OutputParameters
-    )
-
-    args = cp.deepcopy(parser.args)
-    logging.getLogger().setLevel(args.pop("log_level"))
-
-    adata = sc.read_h5ad(args["cell_expression_path"])
-    output = normalize_cell_expresions(adata)
-
-    parser.output(output)
-
-
-if __name__ == "__main__":
-    main()
