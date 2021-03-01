bf9c88dffb2e21349ca1b5f8f156cf678d606f4b
gnayuy <gnayuy@gmail.com>
2021-03-01 13:28:56 -0800

    add dense matrix case for normalization testing

--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -6,20 +6,30 @@ from scipy.sparse import csr_matrix
 
 from transcriptomic_clustering.normalize import normalize_cell_expresions
 
+arr = np.array([[ 6,  6,  4,  2, 18],
+                [ 1,  0,  4,  8, 11],
+                [ 6, 11,  3, 26, 26],
+                [ 0,  0, 19,  9,  0]])
 
-def test_normalization():
-    """
-        test normalization of cell expressions with a small example
-    """
-    arr = np.array([[ 6,  6,  4,  2, 18],
-                    [ 1,  0,  4,  8, 11],
-                    [ 6, 11,  3, 26, 26],
-                    [ 0,  0, 19,  9,  0]])
+expected_norm = np.array([[17.3466147, 17.3466147, 16.7616566, 15.7616695, 18.9315715],
+                    [15.3466407,  0.0000000, 17.3466147, 18.3466104, 18.8060408],
+                    [16.3466234, 17.2210846, 15.3466407, 18.4620873, 18.4620873],
+                    [ 0.0000000,  0.0000000, 19.3721433, 18.2941431,  0.0000000]])
 
-    expected_norm = np.array([[17.3466147, 17.3466147, 16.7616566, 15.7616695, 18.9315715],
-                        [15.3466407,  0.0000000, 17.3466147, 18.3466104, 18.8060408],
-                        [16.3466234, 17.2210846, 15.3466407, 18.4620873, 18.4620873],
-                        [ 0.0000000,  0.0000000, 19.3721433, 18.2941431,  0.0000000]])
+def test_normalization_dense_matrix():
+    """
+        test normalization of cell expressions with a dense matrix
+    """
+
+    a_data = ad.AnnData(arr)
+    result = normalize_cell_expresions(a_data)
+
+    np.testing.assert_almost_equal(result.X, expected_norm, decimal = 6)
+
+def test_normalization_csr_matrix():
+    """
+        test normalization of cell expressions with a sparse csr_matrix
+    """
 
     a_data = ad.AnnData(arr)
     a_data.X = csr_matrix(a_data.X)
