c3d957d16ea4b4b60b4f5342112c79036d8aabab
gnayuy <gnayuy@gmail.com>
2021-03-17 10:39:35 -0700

    reorg select highly variable genes as discussed

--- transcriptomic_clustering/select_highly_variable_genes.py
+++ transcriptomic_clustering/select_highly_variable_genes.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional, List, Literal
 
 import numpy as np
 import pandas as pd
@@ -9,8 +9,6 @@ from skmisc.loess import loess, loess_anova
 from scipy.stats import rankdata, norm
 
 from statsmodels.stats.multitest import fdrcorrection
-
-import functools
 
 def compute_z_scores(dispersion: np.array):
     """
@@ -32,10 +30,103 @@ def compute_z_scores(dispersion: np.array):
     
     return (dispersion  - m_iqr) / delta
 
-def select_hvg(ad_norm:ad.AnnData, selected_cells:Optional[List], 
-                low_thresh: Optional[int] = 1, min_cells: Optional[int] = 4, max_genes: Optional[int] = 3000):
+def hicat_hvg(mtx: csr_matrix, genes: List[str], max_genes: Optional[int] = 3000):
     """
-        select highly variable genes using Brennecke's method
+        select highly variable genes
+
+        Parameters
+        ----------
+        mtx: transposed norm of cell expressions, rows correspond to genes and columns to cells
+        genes: list of genes
+        max_genes: max number of genes to be selected, default is 3000
+
+        Returns
+        -------
+        highly variable genes: list of genes, with means and dispersions
+
+    """
+
+    mtx.data = 2**mtx.data -1
+
+    # means
+    means = np.squeeze(np.asarray(mtx.mean(axis=1)))
+
+    # variances
+    mtx_square = mtx.copy()
+    mtx_square.data **= 2
+    variances = np.squeeze(np.asarray(mtx_square.mean(axis=1))) - np.square(means)
+
+    #  dispersions
+    dispersions = np.log10(variances / (means + 1e-10))
+
+    # z-scores
+    z_scores = compute_z_scores(dispersions)
+
+    # Loess regression
+    positive_filter = (not np.isnan(dispersions).all()) and (dispersions > 0)
+
+    means_filtered = means[positive_filter]
+    dispersions_filtered = dispersions[positive_filter]
+    genes_filtered = genes[positive_filter]
+
+    x = np.log10(means_filtered)
+    y = dispersions_filtered
+
+    loess_regression = loess(x, y)
+    loess_regression.fit()
+
+    loess_fit = loess_regression.outputs
+
+    # p values
+    loess_z = compute_z_scores(loess_fit.fitted_residuals)
+    p_vals = 1 - norm.cdf(loess_z)
+    
+    # p.adjust using BH method
+    p_adj = fdrcorrection(p_vals)[1]
+
+    # select highly variable genes
+    indices = [i for i, x in enumerate(p_adj) if x < 1]
+
+    df = pd.DataFrame(index=indices)
+
+    df['gene'] = genes_filtered[indices]
+    df['p_adj'] = p_adj[indices]
+    df['z_score'] = z_scores[indices]
+    
+    df.sort_values(
+        ['p_adj', 'z_score'],
+        ascending=[True, False],
+        na_position='last',
+        inplace=True,
+    )
+
+    hvg_list = df['gene'][0:max_genes].tolist()
+
+    hvg_dict = {}
+
+    for i in genes:
+        if i in hvg_list:
+            hvg_dict[i] = True
+        else:
+            hvg_dict[i] = False
+
+    hvg_ser = pd.Series(data=hvg_dict, index=hvg_dict.keys())
+
+    df_hvg = pd.DataFrame(index=np.array(genes))
+    df_hvg['means'] = means
+    df_hvg['dispersions'] = dispersions
+    df_hvg['highly_variable'] = hvg_ser
+    
+    return df_hvg
+
+def select_hvg(ad_norm: ad.AnnData, 
+            selected_cells: Optional[List], 
+            low_thresh: Optional[int] = 1, 
+            min_cells: Optional[int] = 4, 
+            max_genes: Optional[int] = 3000,
+            flavor: Literal['hicat', 'seurat', 'cell_ranger'] = 'hicat'):
+    """
+        select highly variable genes
 
         Parameters
         ----------
@@ -47,10 +138,11 @@ def select_hvg(ad_norm:ad.AnnData, selected_cells:Optional[List],
         low_thresh: lower threshold, uesed for pre-select hvg
         min_cells: mininum cells, used for pre-select hvg
         max_genes: number of genes to be selected, default is 3000
+        flavor: highly variable genes method, default uses "hicat"
 
         Returns
         -------
-        highly variable genes: list of genes
+        highly variable genes: added var['highly_variable'] to the the input AnnData
 
     """
 
@@ -69,68 +161,27 @@ def select_hvg(ad_norm:ad.AnnData, selected_cells:Optional[List],
     all_genes = adata_cellsampled.var_names
     pre_selected_genes = all_genes[indices_high].tolist()
 
-    ad_norm_sampled = adata_cellsampled[:, pre_selected_genes]
+    ad_norm_hvg = adata_cellsampled[:, pre_selected_genes]
 
-    # prepare data
-    mtx = ad_norm_sampled.X.transpose()
-    mtx.data = 2**mtx.data-1
+    # hvg
+    if flavor == 'hicat':
 
-    # means
-    means = np.squeeze(np.asarray(mtx.mean(axis=1)))
+        sampled_genes = ad_norm_hvg.var_names
+        df_hvg = hicat_hvg(ad_norm_hvg.X.transpose(), sampled_genes, max_genes)
 
-    # variances
-    mtx_square = mtx.copy()
-    mtx_square.data **= 2
-    variances = np.squeeze(np.asarray(mtx_square.mean(axis=1))) - np.square(means)
+        ad_norm_hvg.uns['hvg'] = {'flavor', 'hicat'}
+        ad_norm_hvg.var['highly_variable'] = df_hvg['highly_variable'].values
+        ad_norm_hvg.var['means'] = df_hvg['means'].values
+        ad_norm_hvg.var['dispersions'] = df_hvg['dispersions'].values
+    
+    elif flavor == 'seurat':
+        sc.pp.highly_variable_genes(ad_norm_hvg, n_top_genes=max_genes)
 
-    #  dispersions
-    dispersions = np.log10(variances / (means + 1e-10))
+    elif flavor == 'cell_ranger':
+        sc.pp.highly_variable_genes(ad_norm_hvg, flavor='cell_ranger', n_top_genes=max_genes)
 
-    # z-scores
-    z_scores = compute_z_scores(dispersions)
+    else:
+        raise ValueError('`flavor` needs to be "hicat", "seurat" or "cell_ranger"')
 
-    # Loess regression
-    not_const = variances > 0
+    return ad_norm_hvg
 
-    x = np.log10(means[not_const])
-    y = dispersions[not_const]
-
-    loess_regression = loess(x, y)
-    loess_regression.fit()
-
-    loess_fit = loess_regression.outputs
-
-    # p values
-    select = (not np.isnan(dispersions).all()) and (dispersions > 0)
-    base = np.min(loess_fit.fitted_values)
-    diff = dispersions - base
-    diff[select] = loess_fit.fitted_residuals[select]
-
-    loess_z = compute_z_scores(diff)
-
-    p_vals = 1 - norm.cdf(loess_z)
-
-    # p.adjust using BH method
-    q_vals = fdrcorrection(p_vals)
-    p_adj = q_vals[1]
-
-    # select highly variable genes
-    indices = [i for i, x in enumerate(p_adj) if x < 1]
-
-    cur_genes = ad_norm_sampled.var_names
-    select_genes_padj = cur_genes[indices].tolist()
-
-    df = pd.DataFrame(index=indices)
-
-    df['gene'] = cur_genes[indices]
-    df['p_adj'] = p_adj[indices]
-    df['z_score'] = z_scores[indices]
-
-    df.sort_values(
-        ['p_adj', 'z_score'],
-        ascending=[True, False],
-        na_position='last',
-        inplace=True,
-    )
-
-    return df['gene'][0:max_genes].tolist()
