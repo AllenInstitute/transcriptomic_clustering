63861fb87edbdb9f0c07812cbd93d1b089d16332
gnayuy <gnayuy@gmail.com>
2021-04-19 16:30:33 -0700

    rm chunked gene_filter and will add them to another branch

--- transcriptomic_clustering/utils.py
+++ transcriptomic_clustering/utils.py
@@ -66,51 +66,6 @@ def select_genes(adata: sc.AnnData,
         selected_genes = list(set(selected_genes))
         adata._inplace_subset_var(selected_genes)
 
-def select_genes_by_chunking(input_cpm_file: str,
-            selected_genes: Optional[List] = None,
-            chunk_size: Optional[int] = 3000):
-    """
-        select genes by chunking
-
-        Parameters
-        ----------
-        input_cpm_file: file name of the CPM normalization of cell expression in AnnData format (csr_matrix is perferred)
-            The annotated data matrix of shape n_obs × n_vars.
-            Rows correspond to cells and columns to genes
-
-        selected_genes: interested genes
-        chunk_size: chunk size
-
-        Returns
-        -------
-        adata with only seleceted genes
-
-    """
-    adata = sc.read_h5ad(input_cpm_file, backed='r')
-    adata.var_names_make_unique()
-
-    for chunk, start, end in adata.chunked_X(chunk_size):
-
-        obs_chunk = adata.obs[start:end]
-        adata_chunk = sc.AnnData(chunk, obs=obs_chunk, var=adata.var)
-        adata_chunk._inplace_subset_var(selected_genes)
-
-        if start == 0:
-            var = adata_chunk.var
-            obs = obs_chunk
-            x_mat = adata_chunk.X
-        else:
-            obs = pd.concat([obs, obs_chunk])
-            x_mat = vstack((x_mat, adata_chunk.X), format='csr')
-
-        del adata_chunk
-
-    adata.file.close()
-
-    adata_genefiltered = sc.AnnData(X=x_mat, obs=obs, var=var)
-
-    return adata_genefiltered
-
 def filter_genes(adata: sc.AnnData,
             low_thresh: Optional[int] = 1,
             min_cells: Optional[int] = 4):
@@ -149,50 +104,3 @@ def filter_genes(adata: sc.AnnData,
     adata.var_names_make_unique()
     adata._inplace_subset_var(pre_selected_genes)
 
-def filter_genes_by_chunking(input_cpm_file: str,
-                            low_thresh: Optional[int] = 1,
-                            min_cells: Optional[int] = 4,
-                            chunk_size: Optional[int] = 3000):
-    """
-        filter genes by chunking
-
-        Parameters
-        ----------
-        input_cpm_file: file name of the log(CPM+1)s normalization cell expression in AnnData format (csr_matrix is perferred)
-            The annotated data matrix of shape n_obs × n_vars.
-            Rows correspond to cells and columns to genes
-
-        low_thresh: lowest value required for a gene to pass filtering.
-        min_cells: minimum number of cells expressed required for a gene to pass filtering.
-        chunk_size: chunk size
-
-        Returns
-        -------
-        filtered genes: list
-
-    """
-    adata = sc.read_h5ad(input_cpm_file, backed='r')
-
-    # init sum of high cpm genes > low threshold
-    sum_hcpm = np.zeros(adata.n_vars).transpose()
-
-    # update sum_hcpm by chunking
-    for chunk, start, end in adata.chunked_X(chunk_size):
-
-        obs_chunk = adata.obs[start:end]
-        adata_chunk = sc.AnnData(chunk, obs=obs_chunk, var=adata.var)
-
-        sum_hcpm_chunk = (adata_chunk.X > low_thresh).sum(axis=0)
-        sum_hcpm += np.squeeze(np.asarray(sum_hcpm_chunk))
-
-        del adata_chunk
-
-    adata.file.close()
-
-    mtx_high = sum_hcpm >= min_cells
-    list_high = mtx_high.tolist()
-    indices_high = [i_gene for i_gene, high_gene in enumerate(list_high) if high_gene == True]
-
-    pre_selected_genes = adata.var_names[indices_high].tolist()
-
-    return list(set(pre_selected_genes))
