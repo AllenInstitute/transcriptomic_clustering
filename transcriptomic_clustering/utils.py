e70c223b0c16407bd8098530140db2ff5bbb9539
gnayuy <gnayuy@gmail.com>
2021-04-27 10:06:44 -0700

    update get_means_variances_genes with filter genes

--- transcriptomic_clustering/utils.py
+++ transcriptomic_clustering/utils.py
@@ -67,43 +67,6 @@ def select_genes(adata: sc.AnnData,
         selected_genes = list(set(selected_genes))
         adata._inplace_subset_var(selected_genes)
 
-def filter_genes_by_thresholds(adata: sc.AnnData,
-            low_thresh: Optional[int] = 1,
-            min_cells: Optional[int] = 4):
-    """
-        filter genes by thresholds
-
-        Parameters
-        ----------
-        adata: log(CPM+1) normalization cell expression in AnnData format (csr_matrix is perferred)
-            The annotated data matrix of shape n_obs × n_vars.
-            Rows correspond to cells and columns to genes
-
-        low_thresh: lowest value required for a gene to pass filtering.
-        min_cells: minimum number of cells expressed required for a gene to pass filtering.
-
-        Returns
-        -------
-        adata with filtered genes
-
-    """
-    if not isinstance(adata, sc.AnnData):
-        raise ValueError('`select_genes` expects an `AnnData` argument')
-
-    mtx_high = (adata.X > low_thresh).sum(axis=0) >= min_cells
-
-    if isinstance(adata.X, csr_matrix):
-        list_high = mtx_high.tolist()[0]
-        indices_high = [i_gene for i_gene, high_gene in enumerate(list_high) if high_gene == True]
-    else:
-        list_high = mtx_high.tolist()
-        indices_high = [i_gene for i_gene, high_gene in enumerate(list_high) if high_gene == True]
-
-    pre_selected_genes = adata.var_names[indices_high].tolist()
-    pre_selected_genes = list(set(pre_selected_genes))
-
-    adata.var_names_make_unique()
-    adata._inplace_subset_var(pre_selected_genes)
 
 def estimate_chunk_size(memory_required_to_run: Optional[int]):
     """
@@ -122,21 +85,28 @@ def get_required_memory_in_GB(adata: sc.AnnData):
     return 5.0 # in GB
 
 
-def get_gene_means_variances(adata: sc.AnnData, chunk_size: Optional[int] = None):
+def get_means_variances_genes(adata: sc.AnnData,
+                            low_thresh: Optional[int] = 1,
+                            min_cells: Optional[int] = 4,
+                            chunk_size: Optional[int] = None):
     """
-        Calculate means and variances for each gene using Welford's online algorithm.
+        Calculate means and variances for each gene using Welford's online algorithm. 
+        And filter genes by thresholds.
 
         Parameters
         ----------
         adata: normalization of cell expression in AnnData format
             The annotated data matrix of shape n_obs × n_vars.
             Rows correspond to cells and columns to genes
+        low_thresh: lowest value required for a gene to pass filtering.
+        min_cells: minimum number of cells expressed required for a gene to pass filtering.
         chunk_size: chunk size
 
         Returns
         -------
         means: numpy array
         variances: numpy array
+        filtered genes: list of genes
 
     """
     memory_required_to_run = get_required_memory_in_GB(adata)
@@ -145,11 +115,34 @@ def get_gene_means_variances(adata: sc.AnnData, chunk_size: Optional[int] = None
         chunk_size = estimate_chunk_size(memory_required_to_run)
 
     if chunk_size >= adata.n_obs:
-        return sc.pp._utils._get_mean_var(np.expm1(adata.X[()]), axis=0)
+        
+        means, variances = sc.pp._utils._get_mean_var(np.expm1(adata.X[()]), axis=0)
+        
+        mtx_high = (adata.X[()] > low_thresh).sum(axis=0) >= min_cells
+        
+        if hasattr(adata.X, "format_str"):
+            if adata.X.format_str == "csr":
+                list_high = mtx_high.tolist()[0]
+            else:
+                list_high = mtx_high.tolist()
+        else:
+            if issparse(adata.X):
+                list_high = mtx_high.tolist()[0]
+            else:
+                list_high = mtx_high.tolist()
+            
+        indices_high = [i_gene for i_gene, high_gene in enumerate(list_high) if high_gene == True]
+        pre_selected_genes = adata.var_names[indices_high].tolist()
+
+        return means, variances, list(set(pre_selected_genes))
     else:
         w_mat = Welford()
+        sum_hcpm = np.zeros(adata.n_vars).transpose()
 
         for chunk, start, end in adata.chunked_X(chunk_size):
+            
+            sum_hcpm_chunk = (chunk > low_thresh).sum(axis=0)
+            sum_hcpm += np.squeeze(np.asarray(sum_hcpm_chunk))
 
             if issparse(chunk):
                 if isinstance(chunk, csr_matrix):
@@ -158,5 +151,11 @@ def get_gene_means_variances(adata: sc.AnnData, chunk_size: Optional[int] = None
                     raise ValueError("Unsupported format for cell_expression matrix. Must be in CSR or dense format")
             else:
                 w_mat.add_all(np.expm1(chunk))
+                
+        mtx_high = sum_hcpm >= min_cells
+        list_high = mtx_high.tolist()
+        indices_high = [i_gene for i_gene, high_gene in enumerate(list_high) if high_gene == True]
 
-        return w_mat.mean, w_mat.var_p
+        pre_selected_genes = adata.var_names[indices_high].tolist()
+
+        return w_mat.mean, w_mat.var_p, list(set(pre_selected_genes))
