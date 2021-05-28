326bb98030c0aea073fed88fc26c97daec62b773
Zach Madigan <zach.madigan@alleninstitute.org>
2021-05-27 19:07:24 -0700

    Don't sort cluster labels

--- transcriptomic_clustering/cluster_means.py
+++ transcriptomic_clustering/cluster_means.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 
 import anndata as ad
 import numpy as np
@@ -112,13 +112,13 @@ def get_cluster_means_backed(
     else:
         chunk_size = estimated_chunk_size
 
-    sorted_cluster_labels = sorted(cluster_assignments.keys())
+    cluster_labels = list([k for k in cluster_assignments.keys()])
 
     # Get cluster X cell to calculate cluster sums. This allows us to do a chunked calculation of cluster X cell @ cell X gene
-    one_hot_cl = get_one_hot_cluster_array(cluster_by_obs, sorted_cluster_labels)
+    one_hot_cl = get_one_hot_cluster_array(cluster_by_obs, cluster_labels)
 
     # Get array of cluster sizes for calculating cluster means
-    cluster_sizes = np.array([len(cluster_assignments[k]) for k in sorted_cluster_labels]).reshape(-1, 1)
+    cluster_sizes = np.array([len(cluster_assignments[k]) for k in cluster_labels]).reshape(-1, 1)
 
     # Calculate cluster and present cluster sums 
     cluster_sums = np.zeros((n_clusters, n_genes))
@@ -136,7 +136,7 @@ def get_cluster_means_backed(
     present_cluster_means = {}
 
     for i in range(n_clusters):
-        k = sorted_cluster_labels[i]
+        k = cluster_labels[i]
         cluster_means[k] = cl_means[i]
         present_cluster_means[k] = present_cl_means[i]
 
@@ -145,7 +145,7 @@ def get_cluster_means_backed(
 
 def get_one_hot_cluster_array(
         cluster_by_obs: np.ndarray,
-        sorted_cluster_labels: np.ndarray
+        cluster_labels: List[Any]
 ) -> np.ndarray:
     """
     Compute a one-hot array of clusters by cells
@@ -154,8 +154,8 @@ def get_one_hot_cluster_array(
     ----------
     cluster_by_obs:
         array of cells with cluster value
-    sorted_cluster_labels:
-        sorted list of cluster labels
+    cluster_labels:
+        list of cluster labels
 
     Returns
     -------
@@ -163,8 +163,8 @@ def get_one_hot_cluster_array(
         one-hot array of cells in a cluster (rows=clusters, columns=cells)
     """
 
-    n_clusters = len(sorted_cluster_labels)
-    cluster_idxs = np.array([sorted_cluster_labels.index(cl) for cl in cluster_by_obs])
+    n_clusters = len(cluster_labels)
+    cluster_idxs = np.array([cluster_labels.index(cl) for cl in cluster_by_obs])
 
     b = np.zeros((cluster_by_obs.size, n_clusters))
     b[np.arange(cluster_by_obs.size), cluster_idxs] = 1
