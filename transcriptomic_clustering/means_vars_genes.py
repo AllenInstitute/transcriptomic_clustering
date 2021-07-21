bda4dd08eff414ad74c24a04469fcc596895739b
Sergey Gratiy <sergeyg@alleninstitute.org>
2021-07-21 09:21:18 -0700

    Refactor get_means_vars_genes
    Add separate backed and inmemory functions

--- transcriptomic_clustering/means_vars_genes.py
+++ transcriptomic_clustering/means_vars_genes.py
@@ -5,6 +5,7 @@ import pandas as pd
 import scanpy as sc
 from scipy.sparse import csr_matrix, issparse
 from welford import Welford
+import warnings
 
 import transcriptomic_clustering as tc
 
@@ -35,43 +36,71 @@ def get_means_vars_genes(adata: sc.AnnData,
     """
 
     if adata.isbacked:
-        # Estimate chunk size
-        if not chunk_size:
-            process_memory_est = adata.n_obs * adata.n_vars * (adata.X.dtype.itemsize / 1024 ** 3)
-            chunk_size = tc.memory.estimate_chunk_size(
-                                adata = adata,
-                                process_memory=process_memory_est,
-                                percent_allowed=25,
-                                process_name='means_vars_genes'
-                            )
+        return get_means_vars_genes_backed(adata, low_thresh, min_cells, chunk_size)
+    else:
+        if chunk_size:
+            warnings.warn("In memory processing does not support chunking. "
+                          "Ignoring `chunk_size` argument.")
 
-        w_mat = Welford()
-        num_cells_above_thresh = np.zeros(adata.n_vars).transpose()
+        return get_means_vars_genes_inmemory(adata, low_thresh, min_cells)
 
-        for chunk, start, end in adata.chunked_X(chunk_size):
 
-            num_cells_above_thresh_chunk = (chunk > low_thresh).sum(axis=0)
-            num_cells_above_thresh += np.squeeze(np.asarray(num_cells_above_thresh_chunk))
+def get_means_vars_genes_inmemory(
+        adata: sc.AnnData,
+        low_thresh: Optional[int] = 1,
+        min_cells: Optional[int] = 4,
 
-            if issparse(chunk):
-                if isinstance(chunk, csr_matrix):
-                    w_mat.add_all(np.expm1(chunk).toarray())
-                else:
-                    raise ValueError("Unsupported format for cell_expression matrix. Must be in CSR or dense format")
-            else:
-                w_mat.add_all(np.expm1(chunk))
+):
+    """
+    Get means, variances and gene mask when data is in memory
+    See description of get_means_vars_genes() for details
+    """
 
-        matrix_gene_mask = num_cells_above_thresh >= min_cells
+    means, variances = sc.pp._utils._get_mean_var(np.expm1(adata.X), axis=0)
+
+    matrix_gene_mask = (adata.X > low_thresh).sum(axis=0) >= min_cells
+
+    if issparse(adata.X):
+        gene_mask = matrix_gene_mask.tolist()[0]
+    else:
         gene_mask = matrix_gene_mask.tolist()
 
-        return w_mat.mean[gene_mask], w_mat.var_p[gene_mask], gene_mask
-    else:
-        means, variances = sc.pp._utils._get_mean_var(np.expm1(adata.X), axis=0)
-            
-        matrix_gene_mask = (adata.X > low_thresh).sum(axis=0) >= min_cells
-        if issparse(adata.X):
-            gene_mask = matrix_gene_mask.tolist()[0]
-        else:
-            gene_mask = matrix_gene_mask.tolist()
+    return means[gene_mask], variances[gene_mask], gene_mask
 
-        return means[gene_mask], variances[gene_mask], gene_mask
+
+def get_means_vars_genes_backed(
+        adata: sc.AnnData,
+        low_thresh: Optional[int] = 1,
+        min_cells: Optional[int] = 4,
+        chunk_size: Optional[int]=None
+):
+    # Estimate chunk size
+    if not chunk_size:
+        process_memory_est = adata.n_obs * adata.n_vars * (adata.X.dtype.itemsize / 1024 ** 3)
+        chunk_size = tc.memory.estimate_chunk_size(
+            adata=adata,
+            process_memory=process_memory_est,
+            percent_allowed=25,
+            process_name='means_vars_genes'
+        )
+
+    w_mat = Welford()
+    num_cells_above_thresh = np.zeros(adata.n_vars).transpose()
+
+    for chunk, start, end in adata.chunked_X(chunk_size):
+
+        num_cells_above_thresh_chunk = (chunk > low_thresh).sum(axis=0)
+        num_cells_above_thresh += np.squeeze(np.asarray(num_cells_above_thresh_chunk))
+
+        if issparse(chunk):
+            if isinstance(chunk, csr_matrix):
+                w_mat.add_all(np.expm1(chunk).toarray())
+            else:
+                raise ValueError("Unsupported format for cell_expression matrix. Must be in CSR or dense format")
+        else:
+            w_mat.add_all(np.expm1(chunk))
+
+    matrix_gene_mask = num_cells_above_thresh >= min_cells
+    gene_mask = matrix_gene_mask.tolist()
+
+    return w_mat.mean[gene_mask], w_mat.var_p[gene_mask], gene_mask
