dbbb44547d7452c517178e00efca6f5c7de74bcd
dyuan1111 <dan.yuan@alleninstitute.org>
2025-07-31 20:54:13 -0700

    update de example

--- examples/pairwiseDE_example.py
+++ examples/pairwiseDE_example.py
@@ -19,22 +19,28 @@ thresholds = {
     'min_genes': 5
 }
 
+# reading in the adata that adata.X is already normalizedd
 adata = sc.read('/allen/programs/celltypes/workgroups/rnaseqanalysis/dyuan/HMBA_analysis/iscANVI_mapping/troubleshoot/test_data/adata_query_MHGlut.h5ad')
 
+# reading in the clustering results, a list of lists of cell indices in adata
 clusters_pth = '/allen/programs/celltypes/workgroups/rnaseqanalysis/dyuan/custom_packages/mpi_tc/archive_versions/v3_githubVersion/test_70k_worked/out/clustering_results.pkl'
 with open(clusters_pth, 'rb') as f:
     clusters = pickle.load(f)
+
+# convert to a dictionary of cluster id to cell indices
 obs_by_cluster = defaultdict(lambda: [])
 for i, cell_ids in enumerate(clusters):
     obs_by_cluster[i] = cell_ids
 
-# subset the adata and obs_by_cluster to only include two clusters for testing
+# subset the adata to include only the first two clusters for testing
 adata = adata[obs_by_cluster[0] + obs_by_cluster[1], :].copy()
 
+# create the new obs_by_cluster of cluster id to the cell indices in the subsetted adata
 obs_by_cluster_subset = {}
 obs_by_cluster_subset[0] = [i for i in range(len(obs_by_cluster[0]))]
 obs_by_cluster_subset[1] = [i + len(obs_by_cluster[0]) for i in range(len(obs_by_cluster[1]))]
 
+# returns a set of markers (20 up regulated and 20 down regulated)
 degs = pairwise_degs(
         adata,
         obs_by_cluster_subset,
